7fcba3937405c2c1ca94459df67167101d5871e9
Shubham_Singh4 <shubham_singh4@dell.com>
2023-08-08 19:01:09 +0530

    LeetCode 121

---
+++ src/slidingWindow/BuySellStock.java
@@ -0,0 +1,32 @@
+package slidingWindow;
+
+// LeetCode 121
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+public class BuySellStock {
+    public static void main(String[] args) {
+        int prices[] = {3,3};
+        System.out.println(maxProfit(prices));
+    }
+
+    static int maxProfit(int[] prices) {
+        if(prices.length < 1){
+            return 0;
+        }
+        int left = 0;
+        int right = 1;
+        int res = 0;
+        int max = prices[right] - prices[left];
+        while(right < prices.length){
+            if(prices[left]<prices[right]){
+                max = prices[right] - prices[left];
+                res = Math.max(res, max);
+                right++;
+            }else{
+                left = right;
+                right++;
+            }
+        }
+        return res;
+    }
+}
